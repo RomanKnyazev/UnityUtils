0e630033390334bd79e1f7124f272d35aaefea4d
knyazev_roman <roman.knyazev@omega-r.com>
2019-09-22 09:57:24 +0300

    Added Screen Autorotaion

---
+++ Autorotation/Autorotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Utils
+{
+    public sealed class Autorotation : MonoBehaviour
+    {
+        public static event System.Action<ScreenOrientation> onOrientationChanged;
+
+        public bool _portrait, _portraitReversed, _landscape, _landscapeReversed;
+
+        private void OnEnable() => Set();
+
+        public void Set() => StartCoroutine(SetInternal());
+
+        private IEnumerator SetInternal()
+        {
+            if (_portrait)
+                Screen.orientation = ScreenOrientation.Portrait;
+            else if (_portraitReversed)
+                Screen.orientation = ScreenOrientation.PortraitUpsideDown;
+            else if (_landscape)
+                Screen.orientation = ScreenOrientation.LandscapeLeft;
+            else if (_landscapeReversed)
+                Screen.orientation = ScreenOrientation.LandscapeRight;
+            
+            var e = onOrientationChanged;
+            if (e != null)
+                e(Screen.orientation);
+
+            yield return null;
+
+            Screen.autorotateToPortrait = _portrait;
+            Screen.autorotateToPortraitUpsideDown = _portraitReversed;
+            Screen.autorotateToLandscapeLeft = _landscape;
+            Screen.autorotateToLandscapeRight = _landscapeReversed;
+            Screen.orientation = ScreenOrientation.AutoRotation;
+        }
+    }   
+}
